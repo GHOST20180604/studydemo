555e79b96251a1b4b5673b1be9d16cd2a845c3a3
hanyc <39898596+GHOST20180604@users.noreply.github.com>
2020-06-11 10:33:46 +0800

    添加redis配置,以及redis工具类

--- src/main/java/com/study/controller/DemoController.java
+++ src/main/java/com/study/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.study.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.study.config.redis.utils.RedisTool;
 import com.study.service.DemoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -18,7 +22,16 @@ public class DemoController {
 
     @Autowired
     private DemoService demoService;
+    @Autowired
+    private RedisTool redisTool;
 
+    /**
+     * 测试分页/测试logger
+     *
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
     @RequestMapping(value = "/getListByPage", method = RequestMethod.GET)
     public Map test2(Integer pageNum, Integer pageSize) {
         Map demoList = demoService.getDemoList(pageNum, pageSize);
@@ -26,6 +39,26 @@ public class DemoController {
         logger.info("logger.info");
         logger.error("logger.error");
         return demoList;
+    }
+
+    /**
+     * 测试redis
+     *
+     * @return
+     */
+    @RequestMapping(value = "/setStringToRedis", method = RequestMethod.GET)
+    public String test3() {
+
+        JSONObject jsonObject = new JSONObject();
+        List<String> list = new ArrayList<>();
+        list.add("list1");
+        list.add("list2");
+        list.add("list3");
+        jsonObject.put("jsonkey1", "josnValue1");
+        redisTool.set("strkey1", "value1");
+        redisTool.hset("hashkey1", "field1", "value1");
+        redisTool.lSet("listkey1", list);
+        return null;
     }
 
 }
